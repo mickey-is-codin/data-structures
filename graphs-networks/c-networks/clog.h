0e047e71d25c3a7c2e43b0db56fba42af1bbe525
Mickey Smith <smithmick14@gmail.com>
2019-09-06 17:26:48 -0500

    Extended to encompass all colors

--- graphs-networks/c-networks/clog.h
+++ graphs-networks/c-networks/clog.h
@@ -16,5 +16,17 @@
 #define RESET "\033[0m"
 
 #define log_red(fmt, ...) printf("%s", RED); printf(fmt, ##__VA_ARGS__); printf("\n%s", RESET);
+#define log_bred(fmt, ...) printf("%s", BRED); printf(fmt, ##__VA_ARGS__); printf("\n%s", RESET);
+#define log_green(fmt, ...) printf("%s", GREEN); printf(fmt, ##__VA_ARGS__); printf("\n%s", RESET);
+#define log_bgreen(fmt, ...) printf("%s", BGREEN); printf(fmt, ##__VA_ARGS__); printf("\n%s", RESET);
+#define log_yell(fmt, ...) printf("%s", YELLOW); printf(fmt, ##__VA_ARGS__); printf("\n%s", RESET);
+#define log_byell(fmt, ...) printf("%s", BYELLOW); printf(fmt, ##__VA_ARGS__); printf("\n%s", RESET);
+#define log_blue(fmt, ...) printf("%s", BLUE); printf(fmt, ##__VA_ARGS__); printf("\n%s", RESET);
+#define log_bblue(fmt, ...) printf("%s", BBLUE); printf(fmt, ##__VA_ARGS__); printf("\n%s", RESET);
+#define log_mag(fmt, ...) printf("%s", MAGENTA); printf(fmt, ##__VA_ARGS__); printf("\n%s", RESET);
+#define log_bmag(fmt, ...) printf("%s", BMAGENTA); printf(fmt, ##__VA_ARGS__); printf("\n%s", RESET);
+#define log_cyan(fmt, ...) printf("%s", CYAN); printf(fmt, ##__VA_ARGS__); printf("\n%s", RESET);
+#define log_bcyan(fmt, ...) printf("%s", BCYAN); printf(fmt, ##__VA_ARGS__); printf("\n%s", RESET);
+
 
 #endif

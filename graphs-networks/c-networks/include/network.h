8a665d961f0824fe09eacbf04f34e98341b8c952
Mickey Smith <smithmick14@gmail.com>
2019-09-06 17:55:32 -0500

    Created parse_args function. Only flag for now is generate-dot

--- graphs-networks/c-networks/include/network.h
+++ graphs-networks/c-networks/include/network.h
@@ -1,4 +1,6 @@
 #ifndef __NETWORK_H__
 #define __NETWORK_H__
 
+void parse_args(int argc, char ** argv, bool * generate_dot);
+
 #endif
--- graphs-networks/c-networks/src/network.c
+++ graphs-networks/c-networks/src/network.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 #include <unistd.h>
 
 #include "../include/network.h"
@@ -11,14 +12,22 @@ int main(int argc, char ** argv) {
 
     printf("Beginning program...\n");
 
+    bool generate_dot = false;
+
+    parse_args(argc, argv, &generate_dot);
+
+    return EXIT_SUCCESS;
+}
+
+void parse_args(int argc, char ** argv, bool * generate_dot) {
+
     int opt;
 
-    while ((opt = getopt(argc, argv, ":if:lrx")) != -1) {
+    while ((opt = getopt(argc, argv, "dick")) != -1) {
         switch (opt) {
-            case 'i':
-            case 'l':
-            case 'r':
-                printf("option: %c\n", opt);
+            case 'd':
+                *generate_dot = true;
+                log_yell("generate-dot specified");
                 break;
             case 'f':
                 printf("filename: %s\n", optarg);
@@ -35,6 +44,4 @@ int main(int argc, char ** argv) {
     for (; optind<argc; optind++) {
         printf("extra argument: %s\n", argv[optind]);
     }
-
-    return EXIT_SUCCESS;
 }

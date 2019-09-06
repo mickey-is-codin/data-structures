d2907990cfdb2adf9ea5e1d6cea4db4c2ec0252e
Mickey Smith <smithmick14@gmail.com>
2019-09-06 17:26:20 -0500

    Created clog test script

---
+++ graphs-networks/c-networks/test_clog.h
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+#include "clog.h"
+
+int main(int argc, char ** argv) {
+
+    log_red("Red");
+    log_bred("Bold Red");
+    log_green("Green");
+    log_bgreen("Bold Green");
+    log_yell("Yellow");
+    log_byell("Bold Yellow");
+    log_blue("Blue");
+    log_bblue("Bold Blue");
+    log_mag("Magenta");
+    log_bmag("Bold Magenta");
+    log_cyan("Cyan");
+    log_bcyan("Bold Cyan");
+
+    return EXIT_SUCCESS;
+}

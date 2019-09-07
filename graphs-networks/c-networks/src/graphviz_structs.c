982d06856b85019fc52e619a08f6ffbe624dfb31
Mickey Smith <smithmick14@gmail.com>
2019-09-06 19:50:27 -0500

    Changed graphs.* -> graph.*

--- graphs-networks/c-networks/src/graphviz_structs.c
+++
@@ -1,4 +0,0 @@
-#ifndef __GVIZ_STRUCTS_H__
-#define __GVIS_STRUCTS_H__
-
-#endif
--- graphs-networks/c-networks/src/network.c
+++ graphs-networks/c-networks/src/network.c
@@ -6,7 +6,8 @@
 #include <time.h>
 
 #include "../include/network.h"
-#include "../include/graphs.h"
+#include "../include/list.h"
+#include "../include/graph.h"
 #include "../include/struct_print.h"
 #include "../include/clog.h"
 
